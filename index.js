e6f5300a8bc1909225e7eb74ebbe3e3ff694195a
twint1m <twint1m.contact@gmail.com>
2023-05-04 11:09:08 +0300

    1 lesson done

---
+++ index.js
@@ -0,0 +1,8 @@
+let title = 1;
+let screens = 2;
+let screenPrice = 3;
+let rollback = 4;
+let fullPrice = 5;
+let adaptiv = 6;
+alert("Lorem ipsum dolor sit amet.");
+console.log("Lorem ipsum dolor sit amet.");
