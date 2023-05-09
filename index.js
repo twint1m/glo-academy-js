d1ad69245ee50e4851eb5a11e108916466fb5f5f
twint1m <twint1m.contact@gmail.com>
2023-05-09 19:49:59 +0300

    fourth lesson done

--- index.js
+++ index.js
@@ -1,40 +1,110 @@
 // const screensArray = screens.toLowerCase().split(" ");
 // const screenPrice = 200;
 // const rollback = 24;
-// const fullPrice = 30000;
-// const adaptive = true;
-// console.log(typeof(fullPrice));
-// console.log(typeof(adaptive));
+
 // console.log(screens.length);
 // console.log("Стоимость верстки экранов"+ " " + screenPrice + " " + "долларов");
 // console.log("Стоимость разработки сайта"+ " " + fullPrice + " " + "долларов");
 // console.log(screensArray);
 // console.log(fullPrice * (rollback/100));
+
+// // объявление переменных
+
+
+
+// let adaptive = confirm('Нужен ли адаптив на сайте?');
+
+// // дополнительные услуги
+
+
+
+// // расчет отката посреднику 
+
+// console.log(servicePercentPrice);
+
+
+
+
+
+
+
+
+
+
+
+// getRollbackMessage()
+// getTitle(title);
+// getServicePercentPrices(servicePercentPrice);
+
+
+
 let title = prompt('Как называется ваш проект?');
 let screens = prompt('Какие типы экранов нужно разработать?');
 let screenPrice = +prompt('Сколько будет стоить данная работа?')
-let adaptive = confirm('Нужен ли адаптив на сайте?');
-// дополнительные услуги
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice1 = +prompt('Сколько это будет стоить?');    
-let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = +prompt('Сколько это будет стоить?'); 
+let service2 = prompt('Какой дополнительный тип услуги нужен?');  
 let servicePrice2 = +prompt('Сколько это будет стоить?');
-
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
-// расчет отката посреднику 
 let servicePercentPrice = Math.ceil(fullPrice - 5000);
-console.log(servicePercentPrice);
+const adaptive = true;
 
+
+console.log(typeof(fullPrice));
+console.log(typeof(adaptive));
 
 
 // расчет скидки
-if (fullPrice >= 30000){
-    console.log('Даем скидку в 10%');
-} else if (fullPrice >= 15000) {
-    console.log('Даем скидку в 5%');
-} else if (fullPrice >= 0) {
-    console.log('Скидка не предусмотрена');
-} else {
-    console.log('Что то пошло не так');
+function getRollbackMessage() {
+    if (fullPrice >= 30000){
+        console.log('Даем скидку в 10%');
+    } else if (fullPrice >= 15000) {
+        console.log('Даем скидку в 5%');
+    } else if (fullPrice >= 0) {
+        console.log('Скидка не предусмотрена');
+    } else {
+        console.log('Что то пошло не так');
+    }
+}   
+
+
+//  задание 1, функция getAllServicePrices
+const servicesPrice = function getAllServicePrices() {
+    return servicePrice1 + servicePrice2;
 }
+const allServicePrices = servicesPrice();
+console.log(allServicePrices);
+
+// задание 2, функция getFullPrice
+function getFullPrice () {
+    return screenPrice + allServicePrices
+}
+const fullPrices = getFullPrice();
+
+
+// задание 3, функция getTitle
+const getTitle = function(title) {
+    let trimmedTitle = title.trim();
+    let slicedLetter = trimmedTitle.slice(0, 1).toUpperCase();
+    let worldLeft = trimmedTitle.slice(1).toLowerCase();
+    let world = slicedLetter + worldLeft;
+    console.log(world);
+}
+// задание 4, функция getServicePercentPrices
+const getServicePercentPrices = function() {
+    return servicePercentPrice
+} 
+const servicePrice = getServicePercentPrices();
+console.log(servicePrice);
+
+getRollbackMessage();
+getTitle(title);
+
+
+
+
+
+
+
+
+
