4fc1694af3f5358a6062320185dafdab92ffb11c
twint1m <twint1m.contact@gmail.com>
2023-05-11 22:19:53 +0300

    fifth lesson done

--- index.js
+++ index.js
@@ -1,58 +1,82 @@
-// const screensArray = screens.toLowerCase().split(" ");
-// const screenPrice = 200;
-// const rollback = 24;
-
-// console.log(screens.length);
-// console.log("Стоимость верстки экранов"+ " " + screenPrice + " " + "долларов");
-// console.log("Стоимость разработки сайта"+ " " + fullPrice + " " + "долларов");
-// console.log(screensArray);
-// console.log(fullPrice * (rollback/100));
-
-// // объявление переменных
-
-
-
-// let adaptive = confirm('Нужен ли адаптив на сайте?');
-
-// // дополнительные услуги
-
-
-
-// // расчет отката посреднику 
-
-// console.log(servicePercentPrice);
-
-
-
-
-
-
-
-
-
-
-
-// getRollbackMessage()
-// getTitle(title);
-// getServicePercentPrices(servicePercentPrice);
-
-
-
+let screens
+let screenPrice
 let title = prompt('Как называется ваш проект?');
-let screens = prompt('Какие типы экранов нужно разработать?');
-let screenPrice = +prompt('Сколько будет стоить данная работа?')
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice1 = +prompt('Сколько это будет стоить?'); 
-let service2 = prompt('Какой дополнительный тип услуги нужен?');  
-let servicePrice2 = +prompt('Сколько это будет стоить?');
+let servicePrice1
+let servicePrice2
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = Math.ceil(fullPrice - 5000);
+
+
 const adaptive = true;
 
 
 console.log(typeof(fullPrice));
 console.log(typeof(adaptive));
 
+
+  
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+// проверка на число servicePrice1
+do {
+    servicePrice1 = prompt('Сколько это будет стоить?'); 
+} while (isNaN(servicePrice1) || servicePrice1.trim() === "" || servicePrice1 === null);
+
+let service2 = prompt('Какой дополнительный тип услуги нужен?');  
+// проверка на число servicePrice2
+do {
+    servicePrice2 = prompt('Сколько это будет стоить?'); 
+} while (isNaN(servicePrice2) || servicePrice2.trim() === "" || servicePrice2 === null);
+
+
+//  4 урок,задание 1, функция getAllServicePrices
+const servicesPrice = function getAllServicePrices() {
+    return servicePrice1 + servicePrice2;
+}
+
+const allServicePrices = servicesPrice();
+console.log(allServicePrices);
+
+// 4 урок,задание 2, функция getFullPrice
+function getFullPrice () {
+    return screenPrice + allServicePrices
+}
+const fullPrices = getFullPrice();
+
+
+// 4 урок,задание 3, функция getTitle
+const getTitle = function(title) {
+    let trimmedTitle = title.trim();
+    let slicedLetter = trimmedTitle.slice(0, 1).toUpperCase();
+    let worldLeft = trimmedTitle.slice(1).toLowerCase();
+    let world = slicedLetter + worldLeft;
+    console.log(world);
+}
+
+getRollbackMessage();
+getTitle(title);
+
+
+
+// 5 урок, задание 1, переменная циклом do while
+do {
+    screens = prompt('Какие типы экранов нужно разработать?');
+    screenPrice = +prompt('Сколько будет стоить данная работа?');
+} while (!screenPrice);
+
+
+// 5 урок, задание 2, функция getServicePercentPrices
+
+
+let servicePrice
+
+const getServicePercentPrices = function() {
+    do {
+      servicePrice = prompt('Сколько это будет стоить?');
+    } while (isNaN(servicePrice) || servicePrice.trim() === "" || servicePrice === null);
+  }; 
+
+  
+
+getServicePercentPrices();
 
 // расчет скидки
 function getRollbackMessage() {
@@ -65,46 +89,4 @@ function getRollbackMessage() {
     } else {
         console.log('Что то пошло не так');
     }
-}   
-
-
-//  задание 1, функция getAllServicePrices
-const servicesPrice = function getAllServicePrices() {
-    return servicePrice1 + servicePrice2;
-}
-const allServicePrices = servicesPrice();
-console.log(allServicePrices);
-
-// задание 2, функция getFullPrice
-function getFullPrice () {
-    return screenPrice + allServicePrices
-}
-const fullPrices = getFullPrice();
-
-
-// задание 3, функция getTitle
-const getTitle = function(title) {
-    let trimmedTitle = title.trim();
-    let slicedLetter = trimmedTitle.slice(0, 1).toUpperCase();
-    let worldLeft = trimmedTitle.slice(1).toLowerCase();
-    let world = slicedLetter + worldLeft;
-    console.log(world);
-}
-// задание 4, функция getServicePercentPrices
-const getServicePercentPrices = function() {
-    return servicePercentPrice
 } 
-const servicePrice = getServicePercentPrices();
-console.log(servicePrice);
-
-getRollbackMessage();
-getTitle(title);
-
-
-
-
-
-
-
-
-
