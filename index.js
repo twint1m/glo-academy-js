7cf530e7467937ab3c8c4adebd9dae60b67ad85a
twint1m <twint1m.contact@gmail.com>
2023-05-15 14:00:00 +0300

    seventh lesson done

--- index.js
+++ index.js
@@ -1,92 +1,72 @@
-let screens
-let screenPrice
-let title = prompt('Как называется ваш проект?');
-let servicePrice1
-let servicePrice2
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const allFunctions = {
+    title: '',
+    screens: '',
+    screenPrice: 0,
+    adaptive: true,
+    rollback: 0,
+    allServicePrices: 0,
+    fullPrice: 0,
+    servicePecrentPrice: 0,
+    service1: '',
+    service2: '',
+    servicePrice1: '',
+    servicePrice2: '',
+    servicePrice: '',
 
 
-const adaptive = true;
+    start: {
+        asking: function () {
+            allFunctions.title = prompt('Как называется ваш проект?', 'Калькулятор верстки');
+            allFunctions.screens = prompt('Какие типы экранов надо разработать?', "мобильный");
+        
+            do {
+                allFunctions.screenPrice = +prompt('Сколько будет стоить данная работа?', '10000');
+            } while (!allFunctions.screenPrice);
+        
+            allFunctions.adaptive = confirm('Нужен ли адаптив на сайте?');
+        }
+    },
+    
 
+    allServicePrices: function getAllServicePrices() {
+        return Number(allFunctions.servicePrice1) + Number(allFunctions.servicePrice2);
+    },
 
-console.log(typeof(fullPrice));
-console.log(typeof(adaptive));
+    fullPrice: function getFullPrice () {
+        return allFunctions.screenPrice + allFunctions.allServicePrices();
+    },
 
+    numberCheck: function() {
+        allFunctions.service1 = prompt('Какой дополнительный тип услуги нужен?', "отправка формы");
+        // проверка на число servicePrice1
+        do {
+            allFunctions.servicePrice1 = prompt('Сколько это будет стоить?', "5000"); 
+        } while (isNaN(allFunctions.servicePrice1) || allFunctions.servicePrice1.trim() === "" || allFunctions.servicePrice1 === null);
+    
+        allFunctions.service2 = prompt('Какой дополнительный тип услуги нужен?', "SEO оптимизация");  
+        // проверка на число servicePrice2
+        do {
+            allFunctions.servicePrice2 = prompt('Сколько это будет стоить?', "5000"); 
+        } while (isNaN(allFunctions.servicePrice2) || allFunctions.servicePrice2.trim() === "" || allFunctions.servicePrice2 === null);
+    },
 
-  
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
-// проверка на число servicePrice1
-do {
-    servicePrice1 = prompt('Сколько это будет стоить?'); 
-} while (isNaN(servicePrice1) || servicePrice1.trim() === "" || servicePrice1 === null);
-
-let service2 = prompt('Какой дополнительный тип услуги нужен?');  
-// проверка на число servicePrice2
-do {
-    servicePrice2 = prompt('Сколько это будет стоить?'); 
-} while (isNaN(servicePrice2) || servicePrice2.trim() === "" || servicePrice2 === null);
-
-
-//  4 урок,задание 1, функция getAllServicePrices
-const servicesPrice = function getAllServicePrices() {
-    return servicePrice1 + servicePrice2;
+    // расчет скидки
+    rollback: function getRollbackMessage() {
+        console.log(allFunctions.fullPrice);
+        if (allFunctions.fullPrice >= 30000){
+            console.log('Даем скидку в 10%');
+        } else if (allFunctions.fullPrice >= 15000) {
+            console.log('Даем скидку в 5%');
+        } else if (allFunctions.fullPrice >= 0) {
+            console.log('Скидка не предусмотрена');
+        } else {
+            console.log('Что то пошло не так');
+        }
+    },
 }
 
-const allServicePrices = servicesPrice();
-console.log(allServicePrices);
-
-// 4 урок,задание 2, функция getFullPrice
-function getFullPrice () {
-    return screenPrice + allServicePrices
-}
-const fullPrices = getFullPrice();
-
-
-// 4 урок,задание 3, функция getTitle
-const getTitle = function(title) {
-    let trimmedTitle = title.trim();
-    let slicedLetter = trimmedTitle.slice(0, 1).toUpperCase();
-    let worldLeft = trimmedTitle.slice(1).toLowerCase();
-    let world = slicedLetter + worldLeft;
-    console.log(world);
-}
-
-getRollbackMessage();
-getTitle(title);
-
-
-
-// 5 урок, задание 1, переменная циклом do while
-do {
-    screens = prompt('Какие типы экранов нужно разработать?');
-    screenPrice = +prompt('Сколько будет стоить данная работа?');
-} while (!screenPrice);
-
-
-// 5 урок, задание 2, функция getServicePercentPrices
-
-
-let servicePrice
-
-const getServicePercentPrices = function() {
-    do {
-      servicePrice = prompt('Сколько это будет стоить?');
-    } while (isNaN(servicePrice) || servicePrice.trim() === "" || servicePrice === null);
-  }; 
-
-  
-
-getServicePercentPrices();
-
-// расчет скидки
-function getRollbackMessage() {
-    if (fullPrice >= 30000){
-        console.log('Даем скидку в 10%');
-    } else if (fullPrice >= 15000) {
-        console.log('Даем скидку в 5%');
-    } else if (fullPrice >= 0) {
-        console.log('Скидка не предусмотрена');
-    } else {
-        console.log('Что то пошло не так');
-    }
-} 
+allFunctions.start.asking();
+allFunctions.allServicePrices();
+allFunctions.fullPrice();
+allFunctions.numberCheck();
+allFunctions.rollback();
